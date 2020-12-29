a3a7b10963dd4c791354f5a289d3689d03ac9dff
wengzepeng <wengzepeng@pcitech.com>
2020-12-29 17:55:03 +0800

    提交服务

---
+++ SlackOff_Service/Authentication/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SlackOff_Service.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ SlackOff_Service/Controllers/AuthenticateController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SlackOff_Service.Authentication;
+
+namespace SlackOff_Service.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticateController
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AuthenticateController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+
+    }
+}
